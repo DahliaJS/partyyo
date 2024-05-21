23be780c1953af03f55a0d56d94ff15f7b48ee28
yuel <jinsik13579@naver.com>
2024-05-21 21:22:05 +0900

    v2.04

--- partyyo/models.py
+++ partyyo/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question', null=True, blank=True)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    voter = models.ManyToManyField(User, related_name='voter_question')
+    voter = models.ManyToManyField(User, related_name='voter_question', null=True, blank=True)
 
     def __str__(self):
         return self.subject
